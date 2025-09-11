78ebc67e18ad036197a497da300c6ebd66709c57
arash.ayatollahi <arashayat@gmail.com>
2025-09-11 14:26:41 +0200

    package hirarchy

--- GenomeUtils/__init__.py
+++ GenomeUtils/__init__.py
@@ -1,24 +1,6 @@
-from .locus import Locus
-from .chromosome import Chromosome
-from .gene import Gene
-from .transcript import Transcript
-from .exon import Exon
-from .genome import Genome
-from .builder import GenomeBuilder
-from .genome_element import GenomeElement
-from .site import Site
-from . import downloaders
+import Genome
 
 
 __all__ = [
-    "Locus",
-    "Chromosome", 
-    "Gene",
-    "Transcript",
-    "Exon",
     "Genome",
-    "GenomeBuilder",
-    "GenomeElement",
-    "Site",
-    "downloaders",
 ]
